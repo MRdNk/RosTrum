a9b7ef3d09726877390913c7788c52ebc583b93c
Duncan Angus Wilkie (mrdnk) <duncanwilkie@hotmail.com>
2013-04-01 16:10:19 +0100

    Game: Added keypress controls, and properly running sheep

--- README.md
+++
@@ -1,8 +0,0 @@
-RosTrum
-=======
-
-RosTrum is a platform WebGL games, using the Canvas Element
-Rostrum is a play of platform
-
-- Just a basic 3D rotating block
-
--- public/javascripts/sprite.js
+++ public/javascripts/sprite.js
@@ -1,4 +1,20 @@
 (function () {
+
+  var KEYS = {
+      LEFT: 37
+    , RIGHT: 39 
+    , UP: 38
+    , DOWN: 40
+  }
+
+  var MOVEMENT = {
+      LEFT: -5
+    , RIGHT: 5
+    , UP: -2
+    , DOWN: 2
+    , RUN_TIME: 200
+  }
+
   var stage = new Kinetic.Stage ({
       container: 'container'
     , width: 1000
@@ -13,7 +29,7 @@
   var animals = ['sheep', 'swan', 'horse', 'dolphin', 'cow', 'dog', 'pig', 'duck'];
   var animations = {};
 
-  for (var i = 0; i<animals.length; i++) {
+  for (var i = 0; i < animals.length; i++) {
     var array = []
 
     for (var x = 0; x < 4; x++) {
@@ -38,55 +54,94 @@
   var imageObj = new Image()
   imageObj.onload = function () {
 
+    Kinetic.Node.addGetterSetter(Kinetic.Sprite, 'directionX', 0);
+    Kinetic.Node.addGetterSetter(Kinetic.Sprite, 'directionY', 0);
+
     var sheep = new Kinetic.Sprite({
-        x: 100
+        x: 120
       , y: 90
       , width: 90
       , image: imageObj
       , animation: animals[animal]
       , animations: animations
       , frameRate: 10
+      , directionX: 0
+      , directionY: 0
     })
-
-    var amplitude = 150;
-    var period = 2000;
-    // in ms
-    var centerX = stage.getWidth() / 2;
 
     background_layer.add(background)
     layer.add(sheep)
     stage.add(background_layer)
     stage.add(layer)
-
-    // console.log(background_layer)
+    
     background_layer.clear()
-    sheep.start()
 
-    var anim = new Kinetic.Animation (function (frame) {
-      var loc = (sheep.getX() > stage.getWidth()) ? 0 - sheep.getWidth() : sheep.getX() + 5;
-      if (sheep.getX() > stage.getWidth()) {
-        if (animal !== animals.length-1) {
-          sheep.setAnimation(animals[++animal])
-          if (animals[animal] === 'dolphin')
-            background_layer.draw()
-          else
-            background_layer.clear() //.setZIndex(0)
+    window.onkeydown = function (e) {
+      switch (e.keyCode) {
+        case KEYS.LEFT: 
+          move(sheep, MOVEMENT.LEFT)
+          break;
+        case KEYS.RIGHT: 
+          move(sheep, MOVEMENT.RIGHT)
+          break;
+        case KEYS.UP:
+          move(sheep, null, MOVEMENT.UP)
+          break;
+        case KEYS.DOWN:
+          move(sheep, null, MOVEMENT.DOWN)
+          break;
 
-        }
-        else {
-          animal = 0;
-          sheep.setAnimation(animals[animal])
-        }
       }
-      sheep.setX (loc)
+    }
 
+    window.onkeyup = function (e) {
+      switch (e.keyCode) {
+        case KEYS.LEFT: 
+          animX.stop()
+          break;
+        case KEYS.RIGHT: 
+          animX.stop()
+          break;
+        case KEYS.UP:
+          animY.stop()
+          break;
+        case KEYS.DOWN:
+          animY.stop()
+          break;
+      }
+      
+      if(!animX.isRunning() && !animY.isRunning()) sheep.stop()
+    }
+
+    function move (shape, x, y) {
+      if (x) { 
+        shape.setDirectionX(x)
+        runAnimation(sheep, 'sheep', true)
+      }
+      if (y) {
+        shape.setDirectionY(y)
+        runAnimation(sheep, 'sheep')
+      }
+      
+    }
+
+    function runAnimation (shape, animation, isX) {
+      if (isX) animX.start()
+      else animY.start()
+    }
+
+    var animX = new Kinetic.Animation (function (frame) {
+      var locX = (sheep.getX() > stage.getWidth()) ? 0 - sheep.getWidth() : sheep.getX() + sheep.getDirectionX();
+      sheep.start()
+      sheep.setX (locX)
     }, layer)
 
-    // sheep.start()
-    anim.start()
-    setTimeout (function () {
-      // anim.stop()
-    }, 2000)
+
+    var animY = new Kinetic.Animation (function (frame) {
+      var locY = (sheep.getY() > stage.getHeight()) ? 0 - sheep.getHeight() : sheep.getY() + sheep.getDirectionY();
+      sheep.start()
+      sheep.setY (locY)
+    }, layer)
   }
 
   imageObj.src = '/images/animals.png'
